4448a845685de5ae152c1b7b73e9487c01f07aab
Ketil Pedersen <ketilpedersen@icloud.com>
2019-11-12 20:41:36 +0100

    Update how menu is intercted with

--- source/navigation/navigation.js
+++ source/navigation/navigation.js
@@ -1,11 +1,29 @@
 export default class Navigation {
   constructor(element) {
-    const menu = element.querySelector(".gryte-menu");
+    const menu = document.querySelector(".gryte-menu");
+    const closeMenu = menu.querySelector(".gryte-menu__close");
+    const main = document.getElementById("gryte-main");
+
     if (menu !== null) {
       element.addEventListener("click", () => {
-        menu.classList.toggle("gryte-menu--active");
+        menu.setAttribute("style", "display:block;");
         menu.setAttribute("aria-hidden", "false");
         menu.setAttribute("aria-expanded", "true");
+
+        if (main !== null) {
+          main.setAttribute("style", "display:none;");
+        }
+      });
+    }
+
+    if (closeMenu !== null) {
+      closeMenu.addEventListener("click", () => {
+        if (menu !== null) {
+          menu.removeAttribute("style");
+        }
+        if (main !== null) {
+          main.removeAttribute("style");
+        }
       });
     }
   }
