b46201cf6bbedf351c816e8f4f20b2ffb331d8d5
Ketil Pedersen <ketilpedersen@icloud.com>
2019-11-13 21:39:28 +0100

    Fix rebind

--- source/util/rebindScripts.js
+++ source/util/rebindScripts.js
@@ -4,10 +4,11 @@ export default function RebindScripts() {
 
   if (script) {
     body.removeChild(script);
-    script.src =
+    const rebinded = document.createElement("script");
+    rebinded.src =
       script.getAttribute("src").split("?")[0] +
       "?v=" +
       Math.floor(Math.random() * 999999);
-    body.appendChild(script);
+    body.appendChild(rebinded);
   }
 }
