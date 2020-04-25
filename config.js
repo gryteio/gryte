f1081001db12e5a68479e6333065b0f0a04fe94c
Ketil Pedersen <ketilpedersen@icloud.com>
2020-04-25 15:20:04 +0200

    Add css path and fix markup

--- config.js
+++ config.js
@@ -4,15 +4,16 @@ const fs = require("fs"),
 module.exports = {
   external: {
     path: process.cwd(),
-    data: loadConfigFile()
+    data: loadConfigFile(),
   },
   src: process.cwd() + "/source",
   scss: path.resolve(__dirname, "source/**/*.scss"),
+  css: path.resolve(__dirname, "dist/gryte.css"),
   html: path.resolve(__dirname, "templates/index.html"),
   template: path.resolve(__dirname, "cli/templates/index.hbs"),
   js: path.resolve(__dirname, "dist/gryte.js"),
   dist: process.cwd() + "/output",
-  logo: path.resolve(__dirname, "assets/logo.svg")
+  logo: path.resolve(__dirname, "assets/logo.svg"),
 };
 
 function loadConfigFile() {
